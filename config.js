4d05a5ded4c7a455a80af54b6a4ddae32deb72fe
Tue99 <86008106+Tue99@users.noreply.github.com>
2021-06-16 22:03:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Đéo thích :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều nhiều 😘😘',
     messDesc: 'Chúc em sinh nhật mãi hạnh phúc và vui tươi bên anh.',
     btnAccept: 'Okiiiii lun <3',
